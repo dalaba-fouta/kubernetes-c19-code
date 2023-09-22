38c68c517ce4975b1ff866cb9e1bf484b0bbd101
Quentin <quentindevial19@gmail.com>
2023-09-22 19:04:41 +0200

    fix linter

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import TheHeader from '@/components/Header.vue';
+import TheHeader from '@/components/TheHeader.vue';
 import { useRouter } from 'vue-router';
 import { useUser } from './shared/stores';
 
@@ -7,24 +7,22 @@ const userStore = useUser();
 const router = useRouter();
 
 async function logout() {
-    await userStore.logout();
-    router.push('/connexion');
+  await userStore.logout();
+  router.push('/connexion');
 }
-
-</script> 
+</script>
 
 <template>
-    <div class="d-flex flex-column app-container">
-        <TheHeader :isAuthenticated="userStore.isAuthenticated" @logout="logout" />
-        <router-view class="flex-fill"></router-view>
-    </div>
+  <div class="d-flex flex-column app-container">
+    <TheHeader :isAuthenticated="userStore.isAuthenticated" @logout="logout" />
+    <router-view class="flex-fill"></router-view>
+  </div>
 </template>
 
-
-<style lang="scss" >
-@import "./assets/base.scss";
+<style lang="scss">
+@import './assets/base.scss';
 
 .app-container {
-    min-height: 100vh;
+  min-height: 100vh;
 }
 </style>
--- frontend/src/shared/services/user.service.ts
+++ frontend/src/shared/services/user.service.ts
@@ -1,27 +1,22 @@
-
-import type { User, UserForm } from "../interfaces";
+import type { User, UserForm } from '../interfaces';
 
 const BASE_URL = '/api/user';
 
 export async function createUser(userForm: UserForm) {
-    try {
-        const response = await fetch(BASE_URL, {
-            method: 'POST',
-            body: JSON.stringify(userForm),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw await response.json();
-        }
-    } catch (e) {
-        throw e;
-    }
+  const response = await fetch(BASE_URL, {
+    method: 'POST',
+    body: JSON.stringify(userForm),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw await response.json();
+  }
 }
 
 export async function fetchCurrentUser(): Promise<User | null> {
-    return await (await fetch(`${BASE_URL}/current`)).json();
+  return await (await fetch(`${BASE_URL}/current`)).json();
 }
